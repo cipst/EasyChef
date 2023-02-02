7e33b80927c35f486b4e5a4b2c5b94086921e35e
Stefano <stefano.cipolletta@outlook.it>
2023-02-02 17:31:14 +0100

    minor fix

--- progetto_finale/js/controller/ingredients.js
+++ progetto_finale/js/controller/ingredients.js
@@ -37,10 +37,10 @@ const handleSubmit = (ingredientName) => {
         onError: (response) => {
             console.log(response);
             let error;
-            if (response.responseJSON.errors[0].toLowerCase().includes("duplicate entry")) {
+            if (response.responseJSON.error.toLowerCase().includes("duplicate entry")) {
                 error = "Ingredient already in the list";
             } else {
-                error = response.responseJSON.errors[0];
+                error = "Error adding the ingredient";
             }
 
             new Alert(ALERT_TYPE.ERROR, "Error", error);
@@ -62,8 +62,8 @@ const getAllIngredients = () => {
                 $(".ingredients-list").append(`<a href="recipes_by_ingredient.php?ingredient=${ingredient.toLowerCase()}">${ingredient}</a>`);
                 $(".ingredients.form-choice").append(`
                     <div key=${index}>
-                        <input style="margin-right: .3em;" type="checkbox" id="${ingredient}" name="ingredient" value="${ingredient}" />
-                        <label for="${ingredient}">${ingredient}</label>
+                        <input style="margin-right: .3em;" type="checkbox" id="ingredient-${ingredient}" name="ingredient" value="${ingredient}" />
+                        <label for="ingredient-${ingredient}">${ingredient}</label>
                     </div>
                 `);
             }
