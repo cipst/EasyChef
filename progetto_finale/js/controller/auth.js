328b45e956a3ba2e832ec6cc4047188436921d86
Stefano <stefano.cipolletta@outlook.it>
2023-02-12 14:12:20 +0100

    auth module

---
+++ progetto_finale/js/controller/auth.js
@@ -0,0 +1,49 @@
+import { Alert } from "../alert.js";
+import { ALERT_TYPE } from "../constants.js";
+import { makeRequest, userLogged } from "../common.js";
+
+$(async () => {
+    // const user = await userLogged();
+
+    testLogin();
+});
+
+function testLogin(who = "topolino") {
+    $(`#tmp-login-${who}`).on("click", () => {
+
+        const textAsBuffer = new TextEncoder().encode(who);
+        window.crypto.subtle.digest('SHA-256', textAsBuffer).then((hashBuffer) => {
+            const hashArray = Array.from(new Uint8Array(hashBuffer));
+            const digest = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+
+            makeRequest({
+                type: "POST",
+                url: "./api/auth/user.php",
+                data: { email: `${who}@gmail.com`, password: digest },
+                onSuccess: (response) => {
+                    const { status } = response;
+                    switch (status) {
+                        case "redirect":
+                            // window.location.href = response.link;
+                            break;
+
+                        case "success":
+                            const user = JSON.parse(response.user);
+                            console.log(user);
+                            if (user === null)
+                                alert(`${who} LOGGED OUT`);
+                            else
+                                alert(`${who} LOGGED IN`);
+                            break;
+
+                        default:
+                            break;
+                    }
+                },
+                onError: (response) => {
+                    console.log("ERROR", response);
+                }
+            });
+        });
+    });
+}
