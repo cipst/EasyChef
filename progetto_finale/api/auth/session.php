7628974af3f8a05b9dbc2bc678dfebc84dd90fa9
Stefano <stefano.cipolletta@outlook.it>
2023-02-12 12:45:00 +0100

    API retrieve the current user from the php session

---
+++ progetto_finale/api/auth/session.php
@@ -0,0 +1,19 @@
+<?php
+require_once("../../php/common.php");
+require_once("../../php/dao/chefs.php");
+
+if (!isset($_SERVER["REQUEST_METHOD"]) || $_SERVER["REQUEST_METHOD"] != "GET")
+    return response(300, "error", ["error" => "Invalid request method!"]);
+
+try {
+    session_start();
+
+    if (!isset($_SESSION["id"]))
+        return response(200, "success", ["user" => json_encode(null)]);
+
+    return response(200, "success", ["user" => json_encode($_SESSION)]);
+} catch (Exception $e) {
+    return response(300, "error", ["error" => $e->getMessage()]);
+} catch (Error $e) {
+    return response(500, "error", ["error" => $e->getMessage()]);
+}
