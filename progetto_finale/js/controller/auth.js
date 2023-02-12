5e3ad169e6264f99351ee077603e843e871b68d3
Stefano <stefano.cipolletta@outlook.it>
2023-02-12 17:16:14 +0100

    login works fine
    TODO: change alert() with something else similar

--- progetto_finale/js/controller/auth.js
+++ progetto_finale/js/controller/auth.js
@@ -7,6 +7,50 @@ $(async () => {
 
     testLogin();
     testLogin("minnie");
+
+    $(".form-container form#login").on("submit", (e) => {
+        e.preventDefault();
+
+        const f = new FormData(e.target);
+        const email = f.get("email");
+        const password = f.get("password");
+
+        const textAsBuffer = new TextEncoder().encode(password);
+        window.crypto.subtle.digest('SHA-256', textAsBuffer).then((hashBuffer) => {
+            const hashArray = Array.from(new Uint8Array(hashBuffer));
+            const digest = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+
+            makeRequest({
+                type: "POST",
+                url: "./api/auth/user.php",
+                data: { email: email, password: digest },
+                onSuccess: (response) => {
+                    const { status } = response;
+                    switch (status) {
+                        case RESPONSE_STATUS.REDIRECT:
+                            // window.location.href = response.link;
+                            break;
+
+                        case RESPONSE_STATUS.OK:
+                            const user = JSON.parse(response.user);
+                            console.log(user);
+                            if (user === null)
+                                alert(`${email} LOGGED OUT`);
+                            else
+                                alert(`${email} LOGGED IN`);
+                            window.location.href = "./index.php";
+                            break;
+
+                        default:
+                            break;
+                    }
+                },
+                onError: (response) => {
+                    console.log("ERROR", response);
+                }
+            });
+        });
+    });
 });
 
 function testLogin(who = "topolino") {
